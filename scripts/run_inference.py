037b7413e27d15082ceaa5596de2b02266c73cd0
Amrita Nallathambi <amrita.nallathambi@skape.bio>
2026-08-20 13:02:51 +0200

    Revert "For fixed chains, retain residue numbering"
    This reverts commit 63e270f715104896a5684108f582046b1d551b1e.

--- scripts/run_inference.py
+++ scripts/run_inference.py
@@ -141,7 +141,6 @@ def main(conf: HydraConfig) -> None:
             sampler.binderlen,
             chain_idx=sampler.chain_idx,
             bfacts=bfacts,
-            idx_pdb=sampler.idx_pdb
         )
 
         # run metadata
